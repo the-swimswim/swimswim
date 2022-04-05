6eb1637873ae50757a2952832d21897ad9a6f367
gbwlxhd97 <gbwlxhd97@naver.com>
2022-04-05 23:47:24 +0900

    일반오디오

--- src/Audio.tsx
+++ src/Audio.tsx
@@ -3,39 +3,39 @@ import ReactAudioPlayer from 'react-audio-player';
 import AudioPlayer from 'react-h5-audio-player';
 function Audio() {
   const player = useRef<any>(null);
-  const [pause,setPause] = useState(false);
-  const onPause = () => {
-    console.log(player.current);
-    pause ? player.current?.onPause() : player.current?.onPlay();
+  const [pause, setPause] = useState(true);
+  const onChangeAudio = () => { //audio handler
+    console.log(player);
     setPause(!pause);
+    !pause ? player.current.play() : player.current.pause();
+
+    
+  };
+  useEffect(() => {
+    player.current.autoplay = true;
+    // player.current;
+    // asyncFunction();
+    console.log('호출이 되엇나요?');
+    console.log(player);
+  },[])
+  const asyncFunction =() => {
+    let playPromise = player.current.play();
+    if(playPromise !== undefined) {
+      playPromise.then((e:any) => {
+        console.log('여기안탐??');
+        
+        // playPromise();
+      })
+      .catch((e:any) => console.log(e))
+    }
   }
+  // player.current.volume = 0.2; volueControl
   //bgm은 껏다 킬 수 있음. onPause, onPlay할때, 각각 전역으로 사진교체
   return (
     <div>
-      {/* 123
-      <AudioPlayer
-        autoPlay
-        src="bgm/TOMBOY.mp3"
-        onPlay={(e) => console.log('onPlay')}
-      /> */}
-      {!pause && <img src="imgs/사운드아이콘_brown.gif" onClick={onPause}/>}
-      {pause && <img src="imgs/사운드아이콘_brown.png" onClick={onPause}/>}
-      <ReactAudioPlayer
-        src="bgm/TOMBOY.mp3"
-        autoPlay
-        controls
-        loop
-        volume={0.2}
-        onPause={() => {
-          setPause(true)
-          console.log('멈춤');
-        }}
-        onPlay={() => {
-          setPause(false);
-          console.log('실행');
-        }}
-        ref={player}
-      />
+      {pause && <img src="imgs/사운드아이콘_brown.gif" onClick={onChangeAudio} />}
+      {!pause && <img src="imgs/사운드아이콘_brown.png" onClick={onChangeAudio} />}
+      <audio src="bgm/TOMBOY.mp3" autoPlay loop ref={player}></audio>
     </div>
   );
 }
