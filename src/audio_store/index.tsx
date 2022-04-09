de3c773266a6c9bdb19b99b6f1ef400860dc0c27
gbwlxhd97 <gbwlxhd97@naver.com>
2022-04-09 18:31:23 +0900

    audio 공통 컴포넌트1차 구현 자동재생은 x

--- src/audio_store/index.tsx
+++ src/audio_store/index.tsx
@@ -5,12 +5,17 @@ export const Context = React.createContext<any>({});
 const Container = (props:any) => {
   const [isPlay, setIsPlay] = useState(true);
   const audioRef = useRef<any>(null)
-  const onClickPlayBtn = () => {
+  const onClickPlayBtn = () => { //bgm control
     setIsPlay(!isPlay);
     !isPlay ? audioRef.current.play() : audioRef.current.pause();
   }
+  const eventSound = (props?:any) => { //일회성 audio control
+    console.log(props);
+    props.current?.play();
+  }
+
   return (
-    <Context.Provider value={{ isPlay, onClickPlayBtn }}>
+    <Context.Provider value={{ isPlay, onClickPlayBtn ,eventSound}}>
       {props.children}
       <audio
         src="bgm/TOMBOY.mp3"
