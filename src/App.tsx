a0519a3e11a0c5ec64fbc2666a2d07620f7fc91a
gbwlxhd97 <gbwlxhd97@naver.com>
2022-04-12 22:42:36 +0900

    vanilla-extract setup && router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import * as styles from "./style.css";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <div className={styles.exampleStyle}>
     <div className="App">
-      쉼쉼 초기셋팅 use Vite Bundler!
+      쉼쉼 초기셋팅 use Vite Bundler
+    </div> 
     </div>
   )
 }
