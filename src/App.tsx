e82621d08811d2befbf5f1690b016ec8eaae767d
kimhyunjo <alsxmqkqh23@gmail.com>
2022-04-15 19:35:18 +0900

    chore: prettier 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 import * as styles from "./style.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className={styles.exampleStyle}>
-    <div className="App">
-      쉼쉼 초기셋팅 use Vite Bundler
-    </div> 
+      <div className="App">쉼쉼 초기셋팅 use Vite Bundler</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
