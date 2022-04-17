17aa5f7bdb6eb2798bdbea8e14862e76666b15fd
ImInnocent <ssje2006@naver.com>
2022-04-17 22:40:25 +0900

    삼단 합체 완료

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import { BrowserRouter,Route, Routes } from 'react-router-dom';
 import AudioContainer from './components/audio/AudioController';
 import Home from './page/Home';
 import Next from './page/Next';
+import TestPage from './page/TestPage';
+import TestContainerPage from './page/TestContainerPage';
+
+import './App.css'
+import "./style.css";
 
 function App() {
 
@@ -11,6 +16,10 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/next" element={<Next />}></Route>
+            <Route path="test">
+              <Route path="" element={<TestPage />} />
+              <Route path=":id" element={<TestContainerPage />} />
+            </Route>
           </Routes>
         </AudioContainer>
     </>
