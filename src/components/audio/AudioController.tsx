a5c8d031f7e376fb024d2cb9ee7dcc247df2dff0
gbwlxhd97 <gbwlxhd97@naver.com>
2022-04-17 19:15:25 +0900

    reducer 뺴고 정리완료

--- src/components/audio/AudioController.tsx
+++ src/components/audio/AudioController.tsx
@@ -1,28 +1,53 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState,useReducer } from 'react';
+import { useEffect } from 'react';
 
 export const Context = React.createContext<any>({});
 
+const audioReducer = (state:any,action:any) => {
+  switch(action?.type) {
+    case '하나씩실행': //두번째 소리가 들어오면 앞에소리 꺼진다.
+      console.log(action);
+      return action.payload;
+    case '동시에실행': //동시에 두가지 이상의 소리가 실행될때.
+      return action.payload; 
+  }
+}
+
 const AudioContainer = (props: any) => {
   const [isPlay, setIsPlay] = useState(false);
-  const audioRef = useRef<any>(null);
+  const bgmRef = useRef<any>(null);
   const onClickBgmBtn = () => { 
-    //bgm control
+    //배경음악 컨트롤
     setIsPlay(!isPlay);
     console.log(isPlay);
-    !isPlay ? audioRef.current.play() : audioRef.current.pause();
+    !isPlay ? bgmRef.current.play() : bgmRef.current.pause();
   };
 
-  const [event, setEvent] = useState(true);
-  const eventSound = (props?: any,eventState?:any) => {
-    //일회성 audio control
-    console.log(props);
-    event ? props.current?.play() : props.current?.pause();
+ // 논의 할것 1:
+ // 1분8초영상 셋타임아웃으로 문여닫이 소리를 재생하려고하는데 문제가 있을까요?
+ // 만약 상단 넘기기 버튼으로 문여닫이 안보면 해당함수 리턴시켜서 종료하기
+  const [viewEvent,dispatch] = useReducer(audioReducer,false);
+  const viewSound = (props: any,stopArg?:any) => {
+    //계속 실행되거나 및 껏다켜야하는 오디오 :일반적으로 풍경 sound가 있다 및 2분20초
+    // viewEvent ? props.current?.play() : props.current?.pause(); 
+    // viewEvent ? console.log('실행') : console.log('멈춤');
+    if (stopArg) {
+      props.current.pause();
+      props.current.currentTime = 0;
+    } else {
+      props.current.play();
+    }
   };
+  
+  // 단순히 이벤트용 오디오 ->실행소리만 동작한다. : 일반적으로 선택 sound가 있다.
+  const startEventSound = (props?:any) => {
+    props.current?.play();
+  }
 
   return (
-    <Context.Provider value={{ isPlay, onClickBgmBtn, eventSound,setEvent,event }}>
+    <Context.Provider value={{ isPlay, onClickBgmBtn, viewSound, dispatch }}>
       {props.children}
-      <audio src="bgm/배경음악.mp3" loop ref={audioRef}></audio>
+      <audio src="bgm/배경음악.mp3" loop ref={bgmRef}></audio>
     </Context.Provider>
   );
 };
--- src/page/Next.tsx
+++ src/page/Next.tsx
@@ -1,29 +1,50 @@
 import React, { useContext, useRef } from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import { Context } from '../components/audio/AudioController';
 import AudioLayout from '../components/Layout';
 
 
 
 const Next = () => {
-  const { eventSound,setEvent,event} = useContext(Context);
+  const { viewSound,dispatch} = useContext(Context);
+  const [selectRef, setSelectRef] = useState<any>() //현재 실행되는 노래를 담고있는 상태
   const childRef = useRef(null);
   const childRef2 = useRef(null);
-
-  const onClickMountin = () => {
-    setEvent(!event);
-    eventSound(childRef);
-  }
   return (
     <AudioLayout>
       <h1>Next</h1>
-      <p onClick={() => onClickMountin()}>
+      <p
+        onClick={() => {
+          viewSound(childRef);
+          setSelectRef(childRef);
+        }}
+      >
         누르면 산소리
+        <img
+          src="imgs/05_계곡커튼.png"
+          style={{ width: '50px', height: '50px' }}
+        />
         <audio src="bgm/산뷰소리.mp3" ref={childRef}></audio>
       </p>
-      <p onClick={() => eventSound(childRef2)}>
+      <p
+        onClick={() => {
+          viewSound(childRef2);
+          setSelectRef(childRef2);
+        }}
+      >
         누르면 계곡소리
+        <img
+          src="imgs/05_커튼_산.png"
+          style={{ width: '50px', height: '50px' }}
+        />
         <audio src="bgm/계곡소리2(은은함).mp3" ref={childRef2}></audio>
       </p>
+      <div onClick={() => {
+        viewSound(selectRef, 'Stop'); //현재 실행되는 음악을 끈다.
+      }}>
+        뒤로가기(누르면원래 다시 커튼3개 선택)
+      </div>
     </AudioLayout>
   );
 };
